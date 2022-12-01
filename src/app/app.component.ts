19b9ac64ed354f76d95b917ed4f654d07a2cd9a2
mgarcas <miggarcia90@gmail.com>
2022-12-02 00:11:22 +0100

    new components and more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: './app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`h3{color:olive}`]
 })
 export class AppComponent {
   title = 'amorcito';
